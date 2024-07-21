6deb27ac33662de21909329c11ca921e2fdfb772
harrison <d.harrison.lima@gmail.com>
2024-07-21 16:26:17 -0300

    feat: add getTransactionByUserId

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,8 +1,8 @@
-import { createTransaction } from "../repositorys/transaction.repository.js";
+import { createTransactionRepository } from "../repositorys/transaction.repository.js";
 
 export const createTransactionController = async (req, res) => {
   try {
-    const transaction = await createTransaction(req.body);
+    const transaction = await createTransactionRepository(req.body);
     console.log(transaction);
     res.status(201).json(transaction);
   } catch (error) {
--- src/repositorys/transaction.repository.js
+++ src/repositorys/transaction.repository.js
@@ -1,8 +1,16 @@
 import { prisma } from "../services/prisma.js";
 
-export const createTransaction = async (transactionBody) => {
+export const createTransactionRepository = async (transactionBody) => {
   const transaction = await prisma.transaction.create({
     data: transactionBody,
   });
   return transaction;
+};
+
+export const getAllTransactionsRespository = async (userId) => {
+  const transactions = await prisma.transaction.findMany({
+    where: {
+      user_id: userId,
+    },
+  });
 };

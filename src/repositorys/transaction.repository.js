f3e34001a7c84942c8980b25e31d6631229b88db
harrison <d.harrison.lima@gmail.com>
2024-07-21 15:18:57 -0300

    feat: add createTransaction in repository

---
+++ src/repositorys/transaction.repository.js
@@ -0,0 +1,17 @@
+import { prisma } from "../services/prisma.js";
+
+export const createTransaction = async (transactionBody) => {
+  const transaction = await prisma.transaction.create({
+    transactionBody,
+    select: {
+      user_id: true,
+      id: true,
+      amount: true,
+      date: true,
+      type: true,
+      name: true,
+    },
+  });
+
+  return transaction;
+};

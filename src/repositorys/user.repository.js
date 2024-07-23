a68740901256777ad39082ed9a63198b46d5c4b7
harrison <d.harrison.lima@gmail.com>
2024-07-22 22:44:37 -0300

    feat: add getUserBalanceRepository

--- src/repositorys/user.repository.js
+++ src/repositorys/user.repository.js
@@ -68,3 +68,53 @@ export const deleteUserRepository = async (id) => {
   });
   return user;
 };
+
+export const getUserBalanceRepository = async (userId) => {
+  const {
+    _sum: { amount: totalExpenses },
+  } = await prisma.transaction.aggregate({
+    where: {
+      user_id: userId,
+      type: "EXPENSE",
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const {
+    _sum: { amount: totalEarnings },
+  } = await prisma.transaction.aggregate({
+    where: {
+      user_id: userId,
+      type: "EARNING",
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const {
+    _sum: { amount: totalInvestments },
+  } = await prisma.transaction.aggregate({
+    where: {
+      user_id: userId,
+      type: "INVESTMENT",
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const _totalEarning = totalEarnings || 0;
+  const _totalExpenses = totalExpenses || 0;
+  const _totalInvestments = totalInvestments || 0;
+  const balance = totalEarnings - totalExpenses - totalInvestments;
+
+  return {
+    earnings: _totalEarning,
+    expenses: _totalExpenses,
+    investments: _totalInvestments,
+    balance,
+  };
+};

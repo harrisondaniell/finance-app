a6e1899ee7f9d51b5b68f15efd4b581ab9c39dae
harrison <d.harrison.lima@gmail.com>
2024-07-22 23:16:07 -0300

    feat: add route get balance by id user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,14 +4,16 @@ import {
   deleteUserController,
   getAllController,
   getByIdController,
+  getUserBalanceController,
   updateUserController,
 } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.post("/user", createUserController);
 userRouter.get("/users", getAllController);
 userRouter.get("/user/:id", getByIdController);
+userRouter.get("/user/:userId/balance", getUserBalanceController);
+userRouter.post("/user", createUserController);
 userRouter.patch("/user/:id", updateUserController);
 userRouter.delete("/user/:id", deleteUserController);
 
